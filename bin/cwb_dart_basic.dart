8dc30af669dc6f13b54b54e732752605ba036350
noanlearn <noviadryn@gmail.com>
2024-01-24 13:53:37 +0700

    first commit

---
+++ bin/cwb_dart_basic.dart
@@ -0,0 +1,5 @@
+import 'package:cwb_dart_basic/cwb_dart_basic.dart' as cwb_dart_basic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cwb_dart_basic.calculate()}!');
+}
---
+++ test/cwb_dart_basic_test.dart
@@ -0,0 +1,8 @@
+import 'package:cwb_dart_basic/cwb_dart_basic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
